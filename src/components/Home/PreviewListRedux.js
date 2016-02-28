0c76abf789efb2bb46dc9cb23ccd442244899a9c
jasonslyvia <jasonslyvia@gmail.com>
2016-02-29 04:13:49 +0800

    prepare data for index

--- src/components/Home/PreviewListRedux.js
+++ src/components/Home/PreviewListRedux.js
@@ -10,13 +10,38 @@ const LOAD_ARTICLES_ERROR = 'LOAD_ARTICLES_ERROR';
 export function loadArticles() {
   return {
     types: [LOAD_ARTICLES, LOAD_ARTICLES_SUCCESS, LOAD_ARTICLES_ERROR],
-    url: '/api/articles'
+    url: '/api/articles.json'
   };
 }
 
 
-export default function home(state = initialState, action) {
+export default function previewList(state = initialState, action) {
   switch (action.type) {
+    case LOAD_ARTICLES: {
+      return {
+        ...state,
+       loading: true,
+        error: false
+      };
+    }
+
+    case LOAD_ARTICLES_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        articleList: action.payload.articleList
+      };
+    }
+
+    case LOAD_ARTICLES_ERROR: {
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
+    }
+
     default:
       return state;
   }
